8c82b3526112a85e8dab38181f9cfabbab2979e9
Eliasssh <kokoumawungoelias.sossah@student.odisee.be>
2025-12-10 10:57:58 +0100

    update

---
+++ IIP1.01.Basis/ConsoleHalloWereld/Program.cs
@@ -0,0 +1,14 @@
+/*
+ * Auteur: Rogier van der Linde
+ * Datum: 2025-09-05
+ */
+using System;
+
+class Program
+{
+   static void Main(string[] args)
+   {
+      // programmacode hier
+      // ...
+   }
+}
